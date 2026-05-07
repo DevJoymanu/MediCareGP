6939d5436c71a9e49b67a3400e00bff9d94e1d20
DevJoymanu <joymanu49@gmail.com>
2026-05-07 10:02:45 +0200

    Add CSRF_TRUSTED_ORIGINS for Railway HTTPS

--- medicaregp/medicaregp/settings.py
+++ medicaregp/medicaregp/settings.py
@@ -7,6 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-medicaregp-dev-key-change-in-production')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', 'https://medicareapp.up.railway.app').split(',')
 INSTALLED_APPS = [
     'django.contrib.admin','django.contrib.auth','django.contrib.contenttypes',
     'django.contrib.sessions','django.contrib.messages','django.contrib.staticfiles',
